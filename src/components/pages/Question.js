0a5b61f02c25ef525748981695dbf3f5199209b4
mariana-martins <mariana.menezes@kathmandu.co.nz>
2020-04-11 17:16:36 +1200

    Connecting question component with Redux

--- src/components/pages/Question.js
+++ src/components/pages/Question.js
@@ -2,17 +2,34 @@ import React from 'react';
 import Loading from '../Loading';
 import Typography from '@material-ui/core/Typography';
 import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 
 function Question() {
   const { id } = useParams();
+  const question = useSelector((state) => state.questions[id]);
+  const user = useSelector((state) => state.users[state.authedUser]);
+
+  const answered = user.answers[id] !== undefined;
   return (
     <>
       <Typography variant={'h4'}>Would you rather?</Typography>
       <Loading>
-        <p>Question {id}</p>
+        {answered ? (
+          <QuestionStats question={question} user={user} />
+        ) : (
+          <AnswerQuestion question={question} user={user} />
+        )}
       </Loading>
     </>
   );
 }
 
 export default Question;
+
+function AnswerQuestion(props) {
+  return <div>lalala</div>;
+}
+
+function QuestionStats(props) {
+  return <div>lelele</div>;
+}
